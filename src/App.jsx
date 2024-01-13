a05844b9a4d6631b0609b1e55ac0da364e510d73
iamjesus <iamjesus.ds@gmail.com>
2024-01-13 17:50:06 -0500

    Added profileForm and Preview component in to App layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,16 @@
+import { ProfileForm, Preview } from './components/organisms'
 
-function App() {
-   return <div className="text-3xl">Title</div>
+function App () {
+  return (
+    <div className='grid grid-cols-2 w-screen h-screen'>
+      <main className='w-full h-full bg-slate-800 p-10'>
+        <ProfileForm />
+      </main>
+      <section className='grid place-items-center'>
+        <Preview />
+      </section>
+    </div>
+  )
 }
 
 export default App
