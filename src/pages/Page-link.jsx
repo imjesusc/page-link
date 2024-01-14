234b023b7d1e4508811812953e6ebfa83ace242a
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 17:28:42 -0500

    Integrate data in PageLink

--- src/pages/Page-link.jsx
+++ src/pages/Page-link.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useStore } from '../store'
 import { decode } from 'js-base64'
+import { PreviewSocialLinks } from '../components/molecules'
+import { Title } from '../components/atoms/Title'
 
 export const PageLink = () => {
   const { profile } = useStore()
-
   const getUrlData = () => {
     const { pathname } = window.location
     if (pathname === '/page-link') return
@@ -26,10 +27,21 @@ export const PageLink = () => {
 
   const data = getUrlData()
   return (
-    <div>
-      <h1>{data?.name}</h1>
-      <p>{data?.description}</p>
-      <img src={data?.avatar} alt='avatar' />
-    </div>
+    <main className='flex flex-col gap-6 p-4 my-10 tablet:my-20'>
+      <article className='grid gap-2 justify-items-center'>
+        {data.avatar &&
+      (
+        <figure className='h-40 w-40 mb-3 rounded-full overflow-hidden ring ring-gray-400 mx-auto'>
+          <img src={data.avatar} className='w-full h-full border-0 object-cover rounded-full' />
+        </figure>
+      )}
+
+        <Title type='h1' className='font-semibold text-center'>{data.name}</Title>
+        <p className='text-lg text-balance text-center text-gray-400 max-w-[500px]'>{data.description}</p>
+      </article>
+      <div>
+        <PreviewSocialLinks profileData={data} />
+      </div>
+    </main>
   )
 }
