530a70e906b8c18b519e2eb2ae52a1f235d2f131
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 00:16:34 -0500

    Refactor usePage hook, enhance encoded data handling

--- src/hooks/usePage.js
+++ src/hooks/usePage.js
@@ -8,14 +8,61 @@ const usePage = () => {
   const encodeData = (value) => encode(value)
   const decodeData = (value) => (value ? decode(value) : '')
 
-  const updateEncodedData = (updatedData) => {
-    // Encode the updated data values
-    const encodedName = encodeData(updatedData.name)
-    const encodedDescription = encodeData(updatedData.description)
-    const encodedAvatar = encodeData(updatedData.avatar)
+  // const metaData = [
+  //   {
+  //     id: 'name',
+  //     label: 'Name'
+  //   },
+  //   {
+  //     id: 'description',
+  //     label: 'Description'
+  //   },
+  //   {
+  //     id: 'avatar',
+  //     label: 'Avatar'
+  //   },
+  //   {
+  //     id: 'facebook',
+  //     label: 'Facebook'
+  //   },
+  //   {
+  //     id: 'twitter',
+  //     label: 'Twitter'
+  //   },
+  //   {
+  //     id: 'instagram',
+  //     label: 'Instagram'
+  //   },
+  //   {
+  //     id: 'github',
+  //     label: 'Github'
+  //   },
+  //   {
+  //     id: 'linkedin',
+  //     label: 'LinkedIn'
+  //   },
+  //   {
+  //     id: 'whatsapp',
+  //     label: 'Whatsapp'
+  //   }
+  // ]
 
-    // Generate the new encoded data string
-    const newEncodedData = `${encodedName}|${encodedDescription}|${encodedAvatar}`
+  const generateEncodedData = (updatedData) => {
+  // Get the keys of the updatedData object
+    const keys = Object.keys(updatedData)
+
+    // Filter out any falsy keys, map them to their encoded values, and join them with '|'
+    const encodedData = keys
+      .filter((key) => key)
+      .map((key) => encodeData(updatedData[key]))
+      .join('|')
+
+    // Return the encoded string
+    return encodedData
+  }
+
+  const updateEncodedData = (updatedData) => {
+    const newEncodedData = generateEncodedData(updatedData)
     // Update the URL with the new encoded data
     window.history.replaceState(null, '', `/${newEncodedData}`)
   }
@@ -23,16 +70,21 @@ const usePage = () => {
   const initData = () => {
     // Extract pathname from window location
     const { pathname } = window.location
+    const keys = Object.keys(profile)
+    const decodedData = {}
 
-    // Split the pathname by '%7C' to get name, description, and avatar
-    const [name, description, avatar] = pathname.slice(1).split('%7C')
+    // Split the pathname by '%7C' to extract the data
+    const data = pathname.slice(1).split('%7C')
 
-    // Decode the extracted values and set them in the profile object
-    setProfile({
-      name: decodeData(name),
-      description: decodeData(description),
-      avatar: decodeData(avatar)
+    if (keys.length !== data.length) {
+      return console.log('Error: Inconsistency in keys and data length.')
+    }
+
+    keys.forEach((key, index) => {
+      decodedData[key] = decodeData(data[index])
     })
+    // Decode the extracted values and set them in the profile object
+    setProfile(decodedData)
   }
 
   useEffect(() => {
