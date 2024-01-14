d918e942f3abcc8210d404bc6f486f85799f5e07
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 00:09:00 -0500

    Added tabler-icons

---
+++ src/components/atoms/Icons.jsx
@@ -0,0 +1,17 @@
+import {
+  IconBrandWhatsapp,
+  IconBrandFacebookFilled,
+  IconBrandX,
+  IconBrandInstagram,
+  IconBrandGithubFilled,
+  IconBrandLinkedin
+} from '@tabler/icons-react'
+
+export const Icons = {
+  BrandWhatsapp: IconBrandWhatsapp,
+  BrandFacebookFilled: IconBrandFacebookFilled,
+  BrandX: IconBrandX,
+  BrandInstagram: IconBrandInstagram,
+  BrandGithubFilled: IconBrandGithubFilled,
+  BrandLinkedin: IconBrandLinkedin
+}
--- src/components/atoms/index.js
+++ src/components/atoms/index.js
@@ -1,3 +1,4 @@
+export * from './Icons'
 export * from './Input'
 export * from './Label'
 export * from './Textarea'
