a1f6c8c1836f4c1f037fed403ffb7707ae61cb08
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 13:06:38 -0500

    Update socialLinks component with CustomInput

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Separator } from './components/atoms/Separator'
 import { ProfileForm, Preview, SocialLinks } from './components/organisms'
 
 function App () {
   return (
     <div className='grid grid-cols-2 w-screen h-screen'>
-      <main className='w-full h-full bg-slate-800 p-10 flex flex-col gap-4'>
+      <main className='w-full h-full bg-slate-800 p-10 flex flex-col gap-10'>
         <ProfileForm />
+        <Separator type='horizontal' />
         <SocialLinks />
       </main>
       <section className='grid place-items-center'>
--- src/components/organisms/SocialLinks.jsx
+++ src/components/organisms/SocialLinks.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Icons, Input, Label } from '../atoms'
 import { usePage } from '../../hooks'
+import { Title } from '../atoms/Title'
 
 const socialMediaData = [
   { id: 'facebook', label: 'Facebook', icon: Icons.BrandFacebookFilled },
@@ -8,21 +9,41 @@ const socialMediaData = [
   { id: 'instagram', label: 'Instagram', icon: Icons.BrandInstagram },
   { id: 'github', label: 'Github', icon: Icons.BrandGithubFilled },
   { id: 'linkedin', label: 'LinkedIn', icon: Icons.BrandLinkedin },
-  { id: 'whatsapp', label: 'Whatsapp', icon: Icons.BrandWhatsapp }
+  { id: 'email', label: 'Email', icon: Icons.BrandEmail }
 ]
 export const SocialLinks = () => {
   const { profile, handleChange } = usePage()
 
+  const handleResetInput = (id) => {
+    handleChange({ target: { id, value: '' } })
+  }
+
   return (
-    <section className='grid grid-cols-2 gap-3'>
-      <div className='col-span-2'>Social Links</div>
-      {socialMediaData?.map((item) => (
-        <div key={item.id}>
-          <Label htmlFor={item.id} text={item.label} />
-          {item.icon && <item.icon />}
-          <Input value={profile[item.id]} type='link' id={item.id} onChange={handleChange} />
-        </div>
-      ))}
+    <section className='grid grid-cols-4 gap-3'>
+      <Title type='h2'>Social Links</Title>
+
+      <div className='grid grid-cols-2 col-span-3 gap-4'>
+        {socialMediaData?.map((item) => (
+          <div key={item.id} className='group'>
+            <Label htmlFor={item.id} text={item.label} />
+            <div className='flex border rounded-lg items-center overflow-hidden'>
+              <span className='min-w-10 p-2 grid place-items-center'>{item.icon && <item.icon className='w-full h-full' />}</span>
+              <Input
+                className='border-0 p-1 rounded-none'
+                value={profile[item.id]}
+                id={item.id}
+                onChange={handleChange}
+              />
+              <button
+                onClick={() => handleResetInput(item.id)}
+                className='h-full p-2 opacity-0 group-hover:opacity-100 grid  duration-500 cursor-pointer w-10 place-items-center'
+              >
+                <Icons.X className='w-full h-full' />
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
     </section>
   )
 }
