248ad690a688516fed6321d05724999d436b4299
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 00:11:40 -0500

    Updated useStore state with new fields

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,13 @@ export const useStore = create((set) => ({
   profile: {
     name: '',
     description: '',
-    avatar: ''
+    avatar: '',
+    facebook: '',
+    twitter: '',
+    instagram: '',
+    github: '',
+    linkedin: '',
+    whatsapp: ''
   },
   setProfile: (profile) => set((state) => ({ profile: { ...state.profile, ...profile } }))
 }))
