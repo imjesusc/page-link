0db35192443d153ccdb34b43f498bdc7c3bdc29c
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 12:09:14 -0500

    Added PreviewSocialLinks component

---
+++ src/components/molecules/PreviewSocialLinks.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { capilaze } from '../../utils'
+import { socialIcons } from '../atoms/Icons'
+
+export const PreviewSocialLinks = ({ profileData }) => {
+  const getSocialLinks = () => {
+    return Object.entries(profileData)
+      .slice(3)
+      .filter(([_, value]) => value)
+      .map(([key, value]) => ({ name: key, url: value, icon: socialIcons[key] }))
+  }
+  return (
+    <ul className='flex items-center justify-center gap-3' aria-label='Social Links'>
+      {
+        getSocialLinks()?.map((item) => {
+          return item?.url &&
+            <li key={item.name} aria-label={capilaze(item.name)}>
+              <a
+                href={item.url}
+                title={capilaze(item.name)}
+                className='block hover:scale-105 transition-transform'
+                target='_blank' rel='noreferrer'
+              >
+                {item.icon && <item.icon className='w-full h-full' />}
+              </a>
+            </li>
+        })
+        }
+    </ul>
+  )
+}
--- src/components/organisms/Preview.jsx
+++ src/components/organisms/Preview.jsx
@@ -1,23 +1,27 @@
 import usePage from '../../hooks/usePage'
+import { Title } from '../atoms/Title'
+import { PreviewSocialLinks } from '../molecules'
 
 export const Preview = () => {
   const { profile } = usePage()
 
   return (
-    <div className='h-[729px] w-[340px] rounded-[50px] px-4 py-6 ring-8 ring-slate-800 overflow-hidden'>
-      <article className='grid gap-3'>
+    <div className='h-[729px] w-[340px] flex flex-col gap-4 rounded-[50px] px-4 py-6 ring-8 ring-slate-800 overflow-hidden'>
+      <article className='grid'>
         {profile.avatar &&
         (
-          <figure className='h-20 w-20 rounded-full overflow-hidden ring ring-gray-400 mx-auto'>
+          <figure className='h-20 w-20 mb-3 rounded-full overflow-hidden ring ring-gray-400 mx-auto'>
             <img src={profile.avatar} className='w-full h-full border-0 object-cover rounded-full' />
           </figure>
         )}
 
-        <h1 className='text-xl  font-semibold text-center'>{profile.name}</h1>
-        <p className='text-balance text-center text-gray-400'>{profile.description.length > 99 ? profile.description.slice(0, 99) + '' : profile.description}</p>
+        <Title type='h2' className='text-xl  font-semibold text-center'>{profile.name}</Title>
+        <p className='text-balance text-center text-gray-400'>{profile.description}</p>
       </article>
 
-      <article />
+      <article>
+        <PreviewSocialLinks profileData={profile} />
+      </article>
     </div>
   )
 }
