9d930cc734eb5a76f3012427619a011f5f2bfe4e
imjesusc <iamjesus.ds@gmail.com>
2024-01-14 17:26:48 -0500

    Added tailwind media query config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./src/**/*.{html,jsx,js}'],
   theme: {
-    extend: {}
+    extend: {
+      screens: {
+        phone: '414px',
+        tablet: '768px',
+        tabletlg: '960px',
+        tabletxl: '1024px',
+        laptop: '1200px',
+        laptoplg: '1400px',
+        desktop: '1700px'
+      }
+    }
   },
   plugins: []
 }
