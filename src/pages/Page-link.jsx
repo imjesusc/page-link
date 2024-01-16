869dcd67c045668cdd4d356d2b058c44851ae1be
imjesusc <iamjesus.ds@gmail.com>
2024-01-15 20:38:10 -0500

    Update page-link styles

--- src/pages/Page-link.jsx
+++ src/pages/Page-link.jsx
@@ -6,6 +6,12 @@ import { useEffect } from 'react'
 export const PageLink = () => {
   const getUrlData = () => {
     const { pathname } = window.location
+    if (pathname === '/page-link/') {
+      return {
+        profile: {},
+        posts: []
+      }
+    }
     const data = pathname.split('page-link/')[1]
     const [profileData, postData] = data.split('posts')
     const decodedProfileData = JSON.parse(decodeData(profileData))
@@ -25,23 +31,23 @@ export const PageLink = () => {
 
   return (
     <main className='flex flex-col gap-6 p-4 my-10 tablet:my-20 max-w-[800px] mx-auto'>
-      <article className='grid justify-items-center tablet:justify-items-start tablet:grid-cols-[auto_1fr] gap-x-10 gap-y-3'>
+      <article className='grid justify-items-center gap-x-10 gap-y-3'>
         {profile.avatar &&
       (
-        <figure className={cn('w-20 h-20 tablet:h-40 tablet:w-40 mb-3 rounded-full',
-          ' overflow-hidden ring ring-gray-400 mx-auto row-span-3')}
+        <figure className={cn('w-24 h-24 tablet:h-40 tablet:w-40 mb-3 rounded-full',
+          ' overflow-hidden ring-4 ring-gray-100/50 mx-auto row-span-3')}
         >
           <img src={profile.avatar} className='w-full h-full border-0 object-cover rounded-full' />
         </figure>
       )}
 
         <Title type='h1' className='text-3xl tablet:text-4xl text-center font-semibold'>{profile.name}</Title>
-        <p className='text-base tablet:text-lg text-pretty text-gray-400 text-center tablet:text-left'>{profile.description}</p>
+        <p className='text-base tablet:text-lg text-pretty text-gray-400 text-center'>{profile.description}</p>
 
         <PreviewSocialLinks className='justify-start' profileData={profile} />
       </article>
 
-      <section className='grid gap-2 mt-10'>
+      <section className={cn('grid gap-2 mt-10', posts.length < 2 ? 'justify-items-center' : '')}>
         {posts.length > 0 && <Title type='h2' className='text-xl font-semibold'>Posts</Title>}
         <PreviewPosts
           posts={posts}
