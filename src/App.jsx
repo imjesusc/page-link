76adb0a6f6f3ad744e7a5ed3c728ada10c7673a7
imjesusc <iamjesus.ds@gmail.com>
2024-02-09 15:59:42 -0500

    feat: Added tailwild config

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
-import { Home } from './pages/Home'
-import { PageLink } from './pages/Page-link'
-function App () {
-  return (
-    <div>
-      <Routes>
-        <Route path='/*' element={<Home />} />
-        <Route path='/page-link/*' element={<PageLink />} />
-      </Routes>
-    </div>
-
-  )
+import { Home, PageLink } from './pages'
+function App() {
+	return (
+		<div>
+			<Routes>
+				<Route path="/*" element={<Home />} />
+				<Route path="/page-link/*" element={<PageLink />} />
+			</Routes>
+		</div>
+	)
 }
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,73 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{html,jsx,js}'],
-  theme: {
-    extend: {
-      screens: {
-        phone: '414px',
-        tablet: '768px',
-        tabletlg: '960px',
-        tabletxl: '1024px',
-        laptop: '1200px',
-        laptoplg: '1400px',
-        desktop: '1700px'
-      }
-    }
-  },
-  plugins: []
+module.exports = {
+	darkMode: 'class',
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}'
+	],
+	theme: {
+		container: {
+			center: true,
+			padding: '1rem',
+			screens: {
+				'2xl': '1400px'
+			}
+		},
+		extend: {
+			boxShadow: {
+				medium: '0px 0px 15px 0px rgba(0,0,0,.03),0px 2px 30px 0px rgba(0,0,0,.08),0px 0px 1px 0px rgba(0,0,0,.3)'
+			},
+			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
+				primary: {
+					DEFAULT: 'hsl(var(--primary))',
+					foreground: 'hsl(var(--primary-foreground))'
+				},
+				secondary: {
+					DEFAULT: 'hsl(var(--secondary))',
+					foreground: 'hsl(var(--secondary-foreground))'
+				},
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))'
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))'
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))'
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))'
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))'
+				}
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)'
+			}
+		},
+		screens: {
+			phone: '414px',
+			tablet: '768px',
+			tabletlg: '960px',
+			tabletxl: '1024px',
+			laptop: '1200px',
+			laptoplg: '1400px',
+			desktop: '1700px'
+		}
+	},
+	plugins: []
 }
